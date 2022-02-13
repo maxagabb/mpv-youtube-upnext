8d295ac1d17abe83bedfd75e361610872d4336a0
maxagabb <maxagabb@gmail.com>
2022-02-12 21:58:28 -0500

    add cursor for current playlist position

--- dmenu-playlist.lua
+++ dmenu-playlist.lua
@@ -12,31 +12,40 @@ local function exec(args, stdin)
     }
     if stdin then command.stdin_data = stdin end
     local ret = mp.command_native(command)
-    return ret.status, ret.stdout, ret
+    return ret.stdout
 end
 
+local function choose_prefix(i)
+	if i == mp.get_property_number('playlist-pos', 0) then
+            return "● "
+        else
+            return "○ "
+        end
+end
 
 local function show_menu()
-
     local playlist = mp.get_property_native('playlist')
     local choiceTable = {}
+
     for i = 1, #playlist do
 	local title = playlist[i].title
 	if(title == nil) then
 		title = playlist[i].filename:match("^.+/(.+)$")
 	end
-        choiceTable[i] = title.."\n"
+        choiceTable[i] = choose_prefix(i-1)..title.."\n"
     end
+
     local choices = table.concat(choiceTable)
     local command = {
 	    "dmenu-dwm",
 	    "-b",
+	    "-i",
 	    "-l",
 	    "40",
 	    "-p",
 	    "___   Search in playlist: "
     }
-    local es, choice, _ = exec(command, choices)
+    local choice = exec(command, choices)
     mp.osd_message(choice)
 
     local selected = -3
